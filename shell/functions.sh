0086af5b44e0c7b5f87ac71470cbc9617b0ee277
Jonathan Chan <cusxio@gmail.com>
2016-07-31 17:01:29 +0800

    color func

--- shell/functions.sh
+++ shell/functions.sh
@@ -56,15 +56,15 @@ j() {
 
 # Docker
 
-function denv() {
+denv() {
     eval "$(docker-machine env "${1:-default}")"
 }
 
-function dic() {
+dic() {
     docker rmi -f $(docker images -q -a -f dangling=true)
 }
 
-function dri() {
+dri() {
     docker rmi $(docker images -q)
 }
 
@@ -72,4 +72,36 @@ function dri() {
 
 save_atom_pkg () {
     apm list --installed --bare > ~/.dotfiles/atom/atom_packages.txt
+}
+
+colorgrid() {
+    iter=16
+    while [ $iter -lt 52 ]
+    do
+        second=$[$iter+36]
+        third=$[$second+36]
+        four=$[$third+36]
+        five=$[$four+36]
+        six=$[$five+36]
+        seven=$[$six+36]
+        if [ $seven -gt 250 ];then seven=$[$seven-251]; fi
+
+        echo -en "\033[38;5;$(echo $iter)m█ "
+        printf "%03d" $iter
+        echo -en "   \033[38;5;$(echo $second)m█ "
+        printf "%03d" $second
+        echo -en "   \033[38;5;$(echo $third)m█ "
+        printf "%03d" $third
+        echo -en "   \033[38;5;$(echo $four)m█ "
+        printf "%03d" $four
+        echo -en "   \033[38;5;$(echo $five)m█ "
+        printf "%03d" $five
+        echo -en "   \033[38;5;$(echo $six)m█ "
+        printf "%03d" $six
+        echo -en "   \033[38;5;$(echo $seven)m█ "
+        printf "%03d" $seven
+
+        iter=$[$iter+1]
+        printf '\r\n'
+    done
 }
