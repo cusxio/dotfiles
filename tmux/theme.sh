92579e566acd58501b73ef0fde927d81eb4f7f5a
Jonathan Chan <cusxio@gmail.com>
2016-07-31 17:01:39 +0800

    tmux theme

---
+++ tmux/theme.sh
@@ -0,0 +1,19 @@
+# Status line
+set -g status-bg default
+set -g status-fg default
+set -g status-left-length 100
+set -g status-left " #[fg=default]cusx#[fg=red] >>#[fg=colour250]%l:%M %p"
+set -g status-right-length 0
+set -g status-right ""
+set -g status-justify right
+setw -g window-status-format '#[fg=colour250]#I|#[fg=colour250]#W'
+setw -g window-status-current-format '#[fg=red]#I|#[fg=magenta]#W'
+
+# Messages
+set -g message-attr underscore
+set -g message-fg black
+set -g message-bg red
+
+# Panes
+set-option -g pane-border-fg colour241
+set-option -g pane-active-border-fg colour51
