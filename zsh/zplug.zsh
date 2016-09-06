35bce0a3406c5656357f44d72af1143741ffca54
Jonathan Chan <cusxio@gmail.com>
2016-09-06 12:13:30 +0800

    themes

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,14 +1,18 @@
 # Sourced by .zshrc
 
-zplug "cusxio/delta-prompt", use:delta.zsh
+# Themes
+# zplug "cusxio/delta-prompt", use:delta.zsh
+zplug "mafredri/zsh-async", on:sindresorhus/pure
+zplug "sindresorhus/pure", use:pure.zsh
 
-zplug "plugins/colored-man-pages", from:oh-my-zsh
-
+# ZSH
 zplug "zsh-users/zsh-syntax-highlighting"
 zplug "zsh-users/zsh-completions"
 # zplug "zsh-users/zsh-autosuggestions"
 
+# Utilities
 zplug "supercrabtree/k"
+zplug "plugins/colored-man-pages", from:oh-my-zsh
 
 # Self managing zplug
 zplug "zplug/zplug"
