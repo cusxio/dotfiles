ec493f2d4514b4d9942ae2e45377f42e3a0b5e26
Jonathan Chan <cusxio@gmail.com>
2016-10-21 23:46:03 +0800

    FZF Color Scheme

--- shell/vars.sh
+++ shell/vars.sh
@@ -23,7 +23,11 @@ export HOMEBREW_NO_ANALYTICS=1
 # FZF
 # https://github.com/junegunn/fzf
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+export FZF_DEFAULT_OPTS='
+  --color prompt:214,hl+:220,pointer:220,hl:214
+'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# export FZF_CTRL_T_OPTS='--preview "(([[ "{}" =~ \.twig$ ]] &&  highlight -S html -O ansi -l {} 2> /dev/null || highlight -O ansi -l {} 2> /dev/null) || cat {} || tree -C {}) 2> /dev/null | head -200" --bind "?:toggle-preview,alt-k:preview-page-up,alt-j:preview-page-down"'
 
 # Go
 export GOPATH="${HOME}/go"
