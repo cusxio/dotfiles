08f789c74b32083ebcd1826c398bd69dec8f7997
Jonathan Chan <cusxio@gmail.com>
2016-10-25 07:53:59 +0800

    update macos keyrepeat config

--- bootstrap/macos.sh
+++ bootstrap/macos.sh
@@ -10,7 +10,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Text Edit automatically opens a new document
