fbf7a98664993cb1a38b5f480ac717b306a28c0f
Jonathan Chan <cusxio@gmail.com>
2016-10-24 09:29:43 +0800

    bin folder init

--- shell/path.sh
+++ shell/path.sh
@@ -2,7 +2,7 @@
 
 # To know the default path in macOS, run
 # man path_helper
-# According to the man, macOS determines the paths 
+# According to the man, macOS determines the paths
 # from /etc/paths
 
 if [[ -n "$OXYGEN_SYSTEM_PATH" ]]; then
@@ -11,12 +11,14 @@ else
     export OXYGEN_SYSTEM_PATH="${PATH}"
 fi
 
-
 COMPOSED_PATH=$OXYGEN_SYSTEM_PATH
 
 COMPOSED_PATH="/usr/local/sbin:${OXYGEN_SYSTEM_PATH}"
 
 # Go
 COMPOSED_PATH="${GOPATH}/bin:${COMPOSED_PATH}"
+
+# Local
+COMPOSED_PATH="${DOTFILES}/bin:${COMPOSED_PATH}"
 
 export PATH=$COMPOSED_PATH
