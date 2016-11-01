a438b6e1dc813c386df5517047c4f5805344fd00
Jonathan Chan <cusxio@gmail.com>
2016-11-01 19:23:17 +0800

    [terminfo] init

---
+++ bootstrap/terminfo.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")/.." || exit 1
+
+declare -r DOTFILES_PATH="$(pwd)"
+
+source "${DOTFILES_PATH}/library/logger.sh"
+
+__terminfo() {
+    logger::action "Setting up terminfo"
+
+    find "${DOTFILES_PATH}/terminfo/" -name '*.terminfo' -exec tic -x {} \;
+}
+
+__terminfo
