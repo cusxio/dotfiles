86c15b1a85795d92f0d651db70f001c203ac11b9
Jonathan Chan <cusxio@gmail.com>
2017-04-11 20:05:12 +0800

    [shell] path comment Postgres.app

--- shell/path.sh
+++ shell/path.sh
@@ -18,7 +18,7 @@ COMPOSED_PATH="/usr/local/sbin:${OXYGEN_SYSTEM_PATH}"
 # Go
 COMPOSED_PATH="${GOPATH}/bin:${COMPOSED_PATH}"
 
-# Postgres
+# Postgres.app
 if [[ -d "/Applications/Postgres.app/Contents/Versions/latest/bin" ]]; then
     COMPOSED_PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:${COMPOSED_PATH}"
 fi
