db87bdda2a3344d016d28319028a8f3d3b0e62d5
Jonathan Chan <cusxio@gmail.com>
2017-04-11 20:05:30 +0800

    [shell] add locale vars

--- shell/vars.sh
+++ shell/vars.sh
@@ -2,6 +2,10 @@
 
 source "${HOME}/.dotfiles/shell/xdg.sh"
 
+# Locale
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+
 # DOTFILES
 export DOTFILES="${HOME}/.dotfiles"
 export ZDOTDIR="${DOTFILES}/zsh"
