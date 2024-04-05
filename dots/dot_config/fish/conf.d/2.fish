563f8fb0dd3f1317771867320a6a695e7df28701
Jonathan Chan <cusxio@gmail.com>
2024-04-05 15:58:26 +0800

    Fix eza args

--- dots/dot_config/fish/conf.d/2.fish
+++ dots/dot_config/fish/conf.d/2.fish
@@ -1,5 +1,5 @@
 if type -q eza
     function ll
-        command eza -Flagh --git --icons --group-directories-first $argv
+        command eza -lagh -F --git --icons --group-directories-first $argv
     end
 end
