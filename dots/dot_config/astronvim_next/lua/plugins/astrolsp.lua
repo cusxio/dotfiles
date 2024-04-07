c282ec8b851552d85011241ece8c133300c7fe42
Jonathan Chan <cusxio@gmail.com>
2024-04-07 23:55:58 +0800

    Use schemastore

--- dots/dot_config/astronvim_next/lua/plugins/astrolsp.lua
+++ dots/dot_config/astronvim_next/lua/plugins/astrolsp.lua
@@ -12,42 +12,6 @@ return {
           },
         },
       },
-      jsonls = {
-        settings = {
-          json = {
-            schemas = {
-              {
-                fileMatch = { "package.json" },
-                url = "https://json.schemastore.org/package.json",
-              },
-              {
-                fileMatch = { "tsconfig*.json" },
-                url = "https://json.schemastore.org/tsconfig.json",
-              },
-              {
-                fileMatch = {
-                  ".prettierrc",
-                  ".prettierrc.json",
-                  "prettier.config.json",
-                },
-                url = "https://json.schemastore.org/prettierrc.json",
-              },
-              {
-                fileMatch = { ".eslintrc", ".eslintrc.json" },
-                url = "https://json.schemastore.org/eslintrc.json",
-              },
-              {
-                fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
-                url = "https://json.schemastore.org/babelrc.json",
-              },
-              {
-                fileMatch = { "now.json", "vercel.json" },
-                url = "https://json.schemastore.org/now.json",
-              },
-            },
-          },
-        },
-      },
       lua_ls = {
         settings = {
           Lua = {
