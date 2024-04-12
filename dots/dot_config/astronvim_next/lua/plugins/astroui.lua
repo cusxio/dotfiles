ec48d3fbc8fb82315b9490f1581a4deb70520a4a
Jonathan Chan <cusxio@gmail.com>
2024-04-12 08:36:14 +0800

    Add mode-text

--- dots/dot_config/astronvim_next/lua/plugins/astroui.lua
+++ dots/dot_config/astronvim_next/lua/plugins/astroui.lua
@@ -1,10 +1,31 @@
 ---@type LazySpec
+-- return {
+--   "AstroNvim/astroui",
+--   opts = function(_, opts)
+--     local colors = require("material.colors")
+--     return require("astrocore").extend_tbl(opts, {
+--       colorscheme = "material",
+--       -- status = {
+--       --   colors = {
+--       --     normal = colors.main.blue,
+--       --   },
+--       -- },
+--     })
+--   end,
+-- }
+
 return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    -- colorscheme = "catppuccin",
     colorscheme = "material",
-    -- colorscheme = "monokai-pro",
+    highlights = {
+      material = function()
+        local colors = require("material.colors")
+        return {
+          HeirlineNormal = { bg = colors.main.blue },
+        }
+      end,
+    },
   },
 }
