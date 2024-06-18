23addd9a5a119021a0cec27a09c096c26d276614
Jonathan Chan <cusxio@gmail.com>
2024-06-18 10:34:39 +0800

    Try flash

---
+++ dots/dot_config/astronvim_next/lua/plugins/flash.lua
@@ -0,0 +1,75 @@
+---@type LazySpec
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          x = {
+            ["s"] = {
+              function()
+                require("flash").jump()
+              end,
+              desc = "Flash",
+            },
+            ["R"] = {
+              function()
+                require("flash").treesitter_search()
+              end,
+              desc = "Treesitter Search",
+            },
+            ["S"] = {
+              function()
+                require("flash").treesitter()
+              end,
+              desc = "Flash Treesitter",
+            },
+          },
+          o = {
+            ["r"] = {
+              function()
+                require("flash").remote()
+              end,
+              desc = "Remote Flash",
+            },
+            ["R"] = {
+              function()
+                require("flash").treesitter_search()
+              end,
+              desc = "Treesitter Search",
+            },
+            ["s"] = {
+              function()
+                require("flash").jump()
+              end,
+              desc = "Flash",
+            },
+            ["S"] = {
+              function()
+                require("flash").treesitter()
+              end,
+              desc = "Flash Treesitter",
+            },
+          },
+          n = {
+            ["s"] = {
+              function()
+                require("flash").jump()
+              end,
+              desc = "Flash",
+            },
+            ["S"] = {
+              function()
+                require("flash").treesitter()
+              end,
+              desc = "Flash Treesitter",
+            },
+          },
+        },
+      },
+    },
+  },
+  opts = {},
+}
