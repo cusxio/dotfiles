f3c74a0565a05b428d30af0051b1f02a9cfed24f
Jonathan Chan <cusxio@gmail.com>
2024-06-21 00:08:24 +0800

    Flash use key instead
    * Adding keymaps in astrocore seems to be adding a lot of delay. The
      keys aren't triggered immediately

--- dots/dot_config/astronvim_next/lua/plugins/flash.lua
+++ dots/dot_config/astronvim_next/lua/plugins/flash.lua
@@ -2,74 +2,48 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  dependencies = {
+  ---@type Flash.Config
+  opts = {},
+  keys = {
     {
-      "AstroNvim/astrocore",
-      opts = {
-        mappings = {
-          x = {
-            ["s"] = {
-              function()
-                require("flash").jump()
-              end,
-              desc = "Flash",
-            },
-            ["R"] = {
-              function()
-                require("flash").treesitter_search()
-              end,
-              desc = "Treesitter Search",
-            },
-            ["S"] = {
-              function()
-                require("flash").treesitter()
-              end,
-              desc = "Flash Treesitter",
-            },
-          },
-          o = {
-            ["r"] = {
-              function()
-                require("flash").remote()
-              end,
-              desc = "Remote Flash",
-            },
-            ["R"] = {
-              function()
-                require("flash").treesitter_search()
-              end,
-              desc = "Treesitter Search",
-            },
-            ["s"] = {
-              function()
-                require("flash").jump()
-              end,
-              desc = "Flash",
-            },
-            ["S"] = {
-              function()
-                require("flash").treesitter()
-              end,
-              desc = "Flash Treesitter",
-            },
-          },
-          n = {
-            ["s"] = {
-              function()
-                require("flash").jump()
-              end,
-              desc = "Flash",
-            },
-            ["S"] = {
-              function()
-                require("flash").treesitter()
-              end,
-              desc = "Flash Treesitter",
-            },
-          },
-        },
-      },
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+    },
+    {
+      "<c-s>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
     },
   },
-  opts = {},
 }
