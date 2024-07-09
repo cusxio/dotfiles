c800a015679530b1adc644a5b1eeb1dba2fb52f4
Jonathan Chan <cusxio@gmail.com>
2024-07-09 10:20:13 +0800

    Normalize specs

--- dots/dot_config/astronvim_next/lua/plugins/lsp-file-operations.lua
+++ dots/dot_config/astronvim_next/lua/plugins/lsp-file-operations.lua
@@ -1,38 +1,40 @@
 ---@type LazySpec
 return {
   "antosha417/nvim-lsp-file-operations",
+  main = "lsp-file-operations",
   init = function(plugin)
     require("astrocore").on_load("neo-tree.nvim", plugin.name)
   end,
-  specs = {
-    "AstroNvim/astrolsp",
-    opts = function(_, opts)
-      local operations = vim.tbl_get(
-        require("astrocore").plugin_opts("nvim-lsp-file-operations"),
-        "operations"
-      ) or {}
-      local fileOperations = {}
-      for _, operation in ipairs({
-        "willRename",
-        "didRename",
-        "willCreate",
-        "didCreate",
-        "willDelete",
-        "didDelete",
-      }) do
-        local enabled = vim.tbl_get(operations, operation .. "Files")
-        if enabled == nil then
-          enabled = true
-        end
-        fileOperations[operation] = enabled
-      end
-      opts.capabilities = vim.tbl_deep_extend(
-        "force",
-        opts.capabilities or {},
-        { workspace = { fileOperations = fileOperations } }
-      )
-    end,
-  },
-  main = "lsp-file-operations",
   opts = {},
+  specs = {
+    {
+      "AstroNvim/astrolsp",
+      opts = function(_, opts)
+        local operations = vim.tbl_get(
+          require("astrocore").plugin_opts("nvim-lsp-file-operations"),
+          "operations"
+        ) or {}
+        local fileOperations = {}
+        for _, operation in ipairs({
+          "willRename",
+          "didRename",
+          "willCreate",
+          "didCreate",
+          "willDelete",
+          "didDelete",
+        }) do
+          local enabled = vim.tbl_get(operations, operation .. "Files")
+          if enabled == nil then
+            enabled = true
+          end
+          fileOperations[operation] = enabled
+        end
+        opts.capabilities = vim.tbl_deep_extend(
+          "force",
+          opts.capabilities or {},
+          { workspace = { fileOperations = fileOperations } }
+        )
+      end,
+    },
+  },
 }
