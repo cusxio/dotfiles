241c1af1046b26d09736e3786db566e4375cc903
Jonathan Chan <cusxio@gmail.com>
2024-07-25 22:44:40 +0800

    Close neovim if neo-tree is last buffer

---
+++ dots/dot_config/lazyvim/lua/config/autocmds.lua
@@ -0,0 +1,27 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#marvinth01
+autocmd("QuitPre", {
+  group = augroup("auto_quit_neo_tree", { clear = true }),
+  callback = function()
+    local tree_wins = {}
+    local floating_wins = {}
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match("neo%-tree") ~= nil then
+        table.insert(tree_wins, w)
+      end
+      if vim.api.nvim_win_get_config(w).relative ~= "" then
+        table.insert(floating_wins, w)
+      end
+    end
+    if 1 == #wins - #floating_wins - #tree_wins then
+      -- Should quit, so we close all invalid windows.
+      for _, w in ipairs(tree_wins) do
+        vim.api.nvim_win_close(w, true)
+      end
+    end
+  end,
+})
