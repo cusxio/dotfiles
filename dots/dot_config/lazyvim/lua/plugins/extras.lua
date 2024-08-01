e848c02a214da054e3ec376f4bc9d11670321f86
Jonathan Chan <cusxio@gmail.com>
2024-08-01 20:56:34 +0800

    Setup python and go via mise

--- dots/dot_config/lazyvim/lua/plugins/extras.lua
+++ dots/dot_config/lazyvim/lua/plugins/extras.lua
@@ -5,11 +5,14 @@ return {
 
   { import = "lazyvim.plugins.extras.lang.astro" },
   { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
-  { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.prisma" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
 
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
